242d7c5434eaa3a65c642e304397db27fc4ea13a
Brooks Johnson <brooksjohnson157@gmail.com>
2021-06-19 05:30:28 -0400

    added google anlytics and simple Vue JS functions.

--- js/intro.js
+++ js/intro.js
@@ -1,3 +1,7 @@
+const salesForceURL = `<a href='https://salesforce.com' target='_blank'>Salesforce</a>`;
+const trailheadURL = `<a href='https//trailheadsalesforce.com' target='_blank'>Trailhead</a>`;
+const vetForceURL = `<a href='https://veterans.force.com/s/' target='_blank'>Vetforce</a>`
+
 const app = Vue.createApp({
     data(){
         return {
@@ -6,15 +10,15 @@ const app = Vue.createApp({
             midlife career tranistion would be. five years later I think I am still plugging away at it.`,
 
             paraTwo: `That first job out of the Army involved working on the Salesforce platform. Nothing too serious but enough to learn what
-            it was. I had never heard of Salesforce in 44 years. At some point I discovered Trailhead and Vetforce, the Salesforce 
+            it was. I had never heard of ${salesForceURL} in 44 years. At some point I discovered ${trailheadURL} and ${vetForceURL}, the Salesforce 
             training program for veterans. And I taught myself to code. This website is really for the self taught programmer and the 
             career changer.            
             `,
 
-            paraThree: `I think that most develper blogs present a shiny finished package and not the messy never ending process that software development 
+            paraThree: `<p>I think that most develper blogs present a shiny finished package and not the messy never ending process that software development 
             is. I wanted a place where I could continue to push myself and share the journey. You can find me a lot on Youtube talking about object oriented
             programing and the Apex language. But I am also very interested in JavaScript and its frameworks, full stack web development, Heroku,
-            Python..... `,
+            Python..... </p>`,
         }
     }
 });

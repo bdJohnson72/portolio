57a136856edbb11e1e5f765379a2af43c3295397
Brooks Johnson <brooksjohnson157@gmail.com>
2021-06-19 23:53:17 -0400

    final version before presentation

--- js/gallery.js
+++ js/gallery.js
@@ -12,6 +12,7 @@ function Gallery(gallery) {
 }
 
 function showImage(el) {
+    console.log('click');
     if (!el) {
         console.info('No image to show');
         return;
@@ -45,8 +46,40 @@ function openModal(){
         return;
     }
     modal.classList.add('open');
+
+    //event listeners bound on open
+    window.addEventListener('keyup', handleKeyUp);
+    nextButton.addEventListener('click', showNextImage);
 }
+
+function closeModal(){
+    modal.classList.remove('open');
+    window.removeEventListener('keyup', handleKeyUp);
+    nextButton.removeEventListener('click', showNextImage);
+    //todo add event listenrs
+}
+
+function handleClickOutside(event){
+    console.log('click');
+    if (event.target === event.currentTarget){
+        closeModal();
+    }
+}
+
+function handleKeyUp(event){
+    if (event.key === 'Escape'){
+        closeModal();
+    }
+}
+
+function showNextImage(){
+    console.log(current.next)
+}
+
 
 images.forEach(image => image.addEventListener('click', event => showImage(event.currentTarget)));
 const gallery1 = Gallery(document.querySelector('.gallery1'));
-const gallery = Gallery(document.querySelector('gallery2'));
+const gallery = document.querySelector('.gallery');
+
+
+modal.addEventListener('click', handleClickOutside);
